afdd5d5f27a0609a40daf4ad038a538f617fef52
IGBx <projetos.r@hotmail.com>
2021-03-29 08:38:07 -0300

    Update index.js'
    I change the method.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const cros = require('cors');
 const express = require('express');
 const app = express();
 const axios = require('axios');
+import api from './api';
 
 app.use(cros());
 
@@ -9,7 +10,7 @@ app.get('/:cep', async(request, response)=>{
 	const { cep } = request.params;
 	console.log('https://ws.apicep.com/cep/'+cep+'.json');
 	try{
-		const { data } = await axios('https://ws.apicep.com/cep/'+cep+'.json');
+		const { data } = await api.get(`${cep}.json`);
 		
 		if(data.status == 400){
 			return response.status(400).json(data);
